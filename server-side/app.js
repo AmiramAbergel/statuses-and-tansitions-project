82ed09dd6244011cd0563f482f4f393bf629f8fb
unknown <Amiram.ab@gmail.com>
2023-07-21 14:41:33 +0300

    chore: server side dependencis added in addition to base app configuration

--- server-side/app.js
+++ server-side/app.js
@@ -1,13 +1,13 @@
-import express, { json } from "express";
-import cors from "cors";
+import express, { json } from 'express';
+import cors from 'cors';
 
-export const app = express()
-const PORT = process.env.PORT || 5000
-const BASE_URL = "/api/v1";
+export const app = express();
+const PORT = process.env.PORT || 5000;
+const BASE_URL = '/api/v1';
 
 app.use(cors());
 app.use(json());
-
+app.use(BASE_URL, indexRoute);
 
 app.listen(PORT, () => {
   console.log(`App is live!`);
