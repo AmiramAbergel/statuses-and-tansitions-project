7a3412dfb51ca3298ad5838355d8023d3be0e077
unknown <Amiram.ab@gmail.com>
2023-07-22 12:51:12 +0300

    chore: emotion installed for css styling

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-const config = require('./babel.config.base');
-
-module.exports = {
-  ...config,
-  babelrcRoots: ['.', 'packages/**/*', 'packages/*', 'units/**/packages/**/*'],
-  sourceType: 'unambiguous',
-};
